7f3a08a8546d60f0837f098d65f48e41dfcafbe9
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2022-07-15 17:23:31 -0500

    Update TopMetricsPilotApp.py

--- TopMetricsPilotApp.py
+++ TopMetricsPilotApp.py
@@ -7,7 +7,7 @@ Created on Sat Jun 18 12:05:48 2022
 import pandas as pd
 import numpy as np
 import streamlit as st
-from mplsoccer.radar_chart2 import Radar
+#from mplsoccer.radar_chart2 import Radar
 import matplotlib.font_manager as font_manager
 import matplotlib.patheffects as path_effects
 import matplotlib.pyplot as plt
